495a820a914b4213c5e092a05662376a9b715044
adantan <killian.dantansentr@gmail.com>
2020-01-22 15:33:30 +0100

    Ajout attribut port, Log en param

--- Server.py
+++ Server.py
@@ -5,13 +5,14 @@ from aioconsole import ainput
 from typing import Dict
 
 class KServer():
-    def __init__(self, port: int):
-        self.__loop: asyncio.BaseEventLoop = asyncio.get_event_loop()
+    def __init__(self, port: int, Log: callable = lambda msg: print("{} : {}".format(time.strftime("[%H:%M:%S]"), msg))):
         self.__server: asyncio.Server = None
-        self.port = port
+        self.loop: asyncio.BaseEventLoop = asyncio.get_event_loop()
+        self.port: int = port
+        self.Log: callable = Log
         self.clts: Dict[asyncio.Transport, object] = {}
     async def __async_init(self):
-        self.__server = await self.__loop.create_server(lambda : KProtocol(self), host = '', port = self.port, family = socket.AF_INET)
+        self.__server = await self.loop.create_server(lambda : KProtocol(self), host = '', port = self.port, family = socket.AF_INET)
         await self.__server.start_serving()
     async def SendInput(self):
         while True:
@@ -20,21 +21,21 @@ class KServer():
                 for clt in list(self.clts.keys()):
                     clt.write(msg.encode("utf8"))
             await asyncio.sleep(0)
-    def Log(msg: str):
-        print("{} : {}".format(time.strftime("[%H:%M:%S]"), msg))
+    # def Log(msg: str):
+    #     print("{} : {}".format(time.strftime("[%H:%M:%S]"), msg))
     def RunServer(self):
-        self.__loop.create_task(self.__async_init())
-        self.__loop.create_task(self.SendInput())
-        self.__loop.run_forever()
+        self.loop.create_task(self.__async_init())
+        #self.loop.create_task(self.SendInput())
+        self.loop.run_forever()
     # -------------------- Protocol callbacks --------------------
     def ConnectionMade(self, transport: asyncio.Transport):
         self.clts[transport] = transport.get_extra_info("peername")
-        KServer.Log("Connexion made : {}".format(self.clts[transport]))
+        self.Log("Connexion made : {}".format(self.clts[transport]))
     def ConnectionLost(self, exc: Exception, transport: asyncio.Transport):
-        KServer.Log("Connexion lost : {}".format(self.clts[transport]))
+        self.Log("Connexion lost : {}".format(self.clts[transport]))
         self.clts.pop(transport)
     def DataReceived(self, data: bytes, transport: asyncio.Transport):
-        KServer.Log(f"Received from {self.clts[transport]} : {data}")
+        self.Log(f"Received from {self.clts[transport]} : {data}")
     # ------------------------------------------------------------
 
 class KProtocol(asyncio.Protocol):
@@ -48,5 +49,3 @@ class KProtocol(asyncio.Protocol):
         self.__server.ConnectionLost(exc, self.__sock_transport)
     def data_received(self, data: bytes):
         self.__server.DataReceived(data, self.__sock_transport)
-
-KServer(55555).RunServer()
