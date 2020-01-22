7022a1fd41044a3f59bd19fdd38eabc28ed66828
killian-dtn <killian.dantansentr@gmail.com>
2020-01-22 19:04:35 +0100

    Fix f string

--- kserver/kserverprotocol.py
+++ kserver/kserverprotocol.py
@@ -3,14 +3,23 @@
 import socket, asyncio as aio, time
 from aioconsole import ainput
 from typing import Dict
+from kserverevent import KServerEvent
 
 class KServer():
-    def __init__(self, port: int, Log: callable = lambda msg: print("{} : {}".format(time.strftime("[%H:%M:%S]"), msg))):
+    def __init__(self, port: int, Log: callable = lambda sender, msg: print("{} : {}".format(time.strftime("[%H:%M:%S]"), msg))):
         self.__server: aio.Server = None
         self.loop: aio.BaseEventLoop = aio.get_event_loop()
         self.port: int = port
         self.Log: callable = Log
         self.clts: Dict[aio.Transport, object] = {}
+        # ------------------------ Events ------------------------
+        self.ConnectionMadeEvent: KServerEvent = KServerEvent()
+        self.ConnectionLostEvent: KServerEvent = KServerEvent()
+        self.DataReceived: KServerEvent = KServerEvent()
+        self.ConnectionMadeEvent += self.Log
+        self.ConnectionLostEvent += self.Log
+        self.DataReceived += self.Log
+        # --------------------------------------------------------
     async def __async_init(self):
         self.__server = await self.loop.create_server(lambda : KProtocol(self), host = '', port = self.port, family = socket.AF_INET)
         await self.__server.start_serving()
@@ -21,21 +30,18 @@ class KServer():
                 for clt in list(self.clts.keys()):
                     clt.write(msg.encode("utf8"))
             await aio.sleep(0)
-    # def Log(msg: str):
-    #     print("{} : {}".format(time.strftime("[%H:%M:%S]"), msg))
     def RunServer(self):
         self.loop.create_task(self.__async_init())
-        #self.loop.create_task(self.SendInput())
         self.loop.run_forever()
     # -------------------- Protocol callbacks --------------------
     def ConnectionMade(self, transport: aio.Transport):
         self.clts[transport] = transport.get_extra_info("peername")
-        self.Log("Connexion made : {}".format(self.clts[transport]))
+        self.ConnectionMadeEvent(self, f"Connexion made : {self.clts[transport]}")
     def ConnectionLost(self, exc: Exception, transport: aio.Transport):
-        self.Log("Connexion lost : {}".format(self.clts[transport]))
+        self.ConnectionLostEvent(self, f"Connexion lost : {self.clts[transport]}")
         self.clts.pop(transport)
     def DataReceived(self, data: bytes, transport: aio.Transport):
-        self.Log(f"Received from {self.clts[transport]} : {data}")
+        self.DataReceived(self, f"Received from {self.clts[transport]} : {data}")
     # ------------------------------------------------------------
 
 class KProtocol(aio.Protocol):
