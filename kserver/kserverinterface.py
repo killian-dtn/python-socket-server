d37597049bc7cf9e7656c8324b1afe2df1fcda10
adantan <killian.dantansentr@gmail.com>
2020-01-23 16:09:09 +0100

    Supression LogEvent, Ajout ExecCommand

--- kserver/kserverinterface.py
+++ kserver/kserverinterface.py
@@ -2,7 +2,7 @@
 
 import tkinter as tk
 import asyncio as aio
-from typing import List
+from typing import List, Dict
 from . import KServer
 
 class KServerInterface(tk.Tk):
@@ -14,34 +14,52 @@ class KServerInterface(tk.Tk):
         self.tasks.append(self.loop.create_task(self.__MainloopAsync()))
         # ------------------------------ Widgets ------------------------------
         self.CommandEntry: tk.Entry = tk.Entry(self)
-        self.ClientsListbox: tk.Listbox = tk.Listbox(self)
+        self.ClientsListbox: tk.Listbox = tk.Listbox(self, selectmode = tk.MULTIPLE)
         # ------------------------------ Events -------------------------------
         self.server.ConnectionMadeEvent += self.ConnectionMade
         self.server.ConnectionLostEvent += self.ConnectionLost
         self.server.DataReceivedEvent += self.DataReceived
         # ---------------------------------------------------------------------
         self.protocol("WM_DELETE_WINDOW", self.Close)
+        self.commands: Dict[str, callable] = {}
+        self.clts: Dict[str, aio.Transport] = {}
     async def __MainloopAsync(self):
         while True:
             self.update()
             await aio.sleep(0)
-    # ---------------------------- Event functions ----------------------------
+    # ------------------------ Server Events functions ------------------------
     def ConnectionMade(self, sender, client: aio.Transport):
-        self.ClientsListbox.insert(tk.END, client)
+        txt: str = str(client.get_extra_info("peername"))
+        self.ClientsListbox.insert(tk.END, txt)
+        self.clts[txt] = client
     def ConnectionLost(self, sender, reason: Exception, client: aio.Transport):
         self.ClientsListbox.delete(0, tk.END)
         self.ClientsListbox.insert(tk.END, list(self.server.clts.keys()))
+        self.clts.pop(str(client.get_extra_info("peername")))
     def DataReceived(self, sender, msg: bytes, client: aio.Transport):
         pass
     # -------------------------------------------------------------------------
+    def ExecCommand(self, command: str):
+        selected_items = [self.ClientsListbox.get(i) for i in self.ClientsListbox.curselection()]
+        try:
+            self.commands[command](targets = selected_items)
+        except KeyError:
+            self.server.Log(msg = f"No command \"{command}\".")
+        except TypeError:
+            raise
+        finally:
+            self.server.Log(msg = f"Command : {command}, on target(s) : {selected_items}")
+        self.CommandEntry.delete(0, tk.END)
     def Render(self):
         self.ClientsListbox.pack(fill = tk.BOTH, expand = True)
         self.CommandEntry.pack(fill = tk.X, expand = False)
+        self.CommandEntry.bind("<Return>", lambda event: self.ExecCommand(self.CommandEntry.get()))
     def Run(self):
         self.Render()
         self.server.RunServer()
     def Close(self):
         for task in self.tasks:
             task.cancel()
+        self.server.StopServer()
         self.loop.stop()
         self.destroy()
--- kserver/kserverprotocol.py
+++ kserver/kserverprotocol.py
@@ -5,7 +5,7 @@ from typing import Dict
 from . import KServerEvent
 
 class KServer():
-    def __init__(self, port: int, Log: callable = lambda sender, msg: print("{} : {}".format(time.strftime("[%H:%M:%S]"), msg))):
+    def __init__(self, port: int, Log: callable = lambda msg: print("{} : {}".format(time.strftime("[%H:%M:%S]"), msg))):
         self.__server: aio.Server = None
         self.loop: aio.BaseEventLoop = aio.get_event_loop()
         self.port: int = port
@@ -15,8 +15,6 @@ class KServer():
         self.ConnectionMadeEvent: KServerEvent = KServerEvent() # args : client
         self.ConnectionLostEvent: KServerEvent = KServerEvent() # args : reason, client
         self.DataReceivedEvent: KServerEvent = KServerEvent()   # args : msg, client
-        self.LogEvent: KServerEvent = KServerEvent()            # args : msg
-        self.LogEvent += self.Log
         # --------------------------------------------------------
     async def __async_init(self):
         self.__server = await self.loop.create_server(lambda : KProtocol(self), host = '', port = self.port, family = socket.AF_INET)
@@ -24,18 +22,20 @@ class KServer():
     def RunServer(self):
         self.loop.create_task(self.__async_init())
         self.loop.run_forever()
+    def StopServer(self):
+        self.__server.close()
     # -------------------- Protocol callbacks --------------------
     def ConnectionMade(self, transport: aio.Transport):
         self.clts[transport] = transport.get_extra_info("peername")
         self.ConnectionMadeEvent(self, client = transport)
-        self.LogEvent(self, msg = f"Connexion made : {self.clts[transport]}")
+        self.Log(msg = f"Connexion made : {self.clts[transport]}")
     def ConnectionLost(self, exc: Exception, transport: aio.Transport):
-        self.LogEvent(self, msg = f"Connexion lost : {self.clts[transport]}")
+        self.Log(msg = f"Connexion lost : {self.clts[transport]}")
         self.clts.pop(transport)
         self.ConnectionLostEvent(self, reason = exc, client = transport)
     def DataReceived(self, data: bytes, transport: aio.Transport):
         self.DataReceivedEvent(self, msg = data, client = transport)
-        self.LogEvent(self, msg = f"Received from {self.clts[transport]} : {data}")
+        self.Log(msg = f"Received from {self.clts[transport]} : {data}")
     # ------------------------------------------------------------
 
 class KProtocol(aio.Protocol):
